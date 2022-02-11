2c1f60c7629fcd53fe6be0dbd7c09d3666e05780
Ayush <tseegii.tseky@gmail.com>
2022-02-11 20:16:19 +0800

    Shoog shiddeg bolgoh

--- app.js
+++ app.js
@@ -7,5 +7,21 @@ var activePlayer = 0;
 //Тоглогчийн ээлжиндээ цуглуулж байгаа оноог хадгалах хувьсагч
 var roundScore = 0;
 //Шооны аль талаараа буусныг хадгалах хувьсагч, 1-6 гэсэн утгыг энэ хувьсагчид санамсаргүйгээр үүсгэж өгнө.
-var dice = Math.floor(Math.random() * 6 + 1);
- console.log(dice);
+//var diceNumber = Math.floor(Math.random() * 6) + 1;
+
+ //Программын эхлэл
+  document.getElementById('score-0').textContent = 0;
+  document.getElementById('score-1').textContent = 0;
+
+  document.getElementById('current-0').textContent = 0;
+  document.getElementById('current-1').textContent = 0;
+var diceDoc = document.querySelector('.dice');
+diceDoc.style.display = "none";
+
+
+  //Event Roll Dice click хийгдэх үед шооны зургийг харгалзуулан гаргана.
+  document.querySelector('.btn-roll').addEventListener('click', function() {
+    var diceNumber = Math.floor(Math.random() * 6) + 1;
+    diceDoc.style.display = "block";
+    diceDoc.src =  "dice-" + diceNumber + ".png";
+  });

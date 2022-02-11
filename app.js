faa80507bafd877aed7f18b7edc34d13ec3e9b17
Ayush <tseegii.tseky@gmail.com>
2022-02-11 22:43:55 +0800

    Toglogchin eeljiig solidog bolgono

--- app.js
+++ app.js
@@ -21,7 +21,30 @@ diceDoc.style.display = "none";
 
   //Event Roll Dice click хийгдэх үед шооны зургийг харгалзуулан гаргана.
   document.querySelector('.btn-roll').addEventListener('click', function() {
+    //1-6 хоорондох тоог санамсагүйгээх гаргаж ирнэ.
     var diceNumber = Math.floor(Math.random() * 6) + 1;
+    //Шооны зургийг гаргаж ирнэ.
     diceDoc.style.display = "block";
+ 
+// Буусан санамсаргүй тоонд харгалзах шооны зургийг гаргаж ирнэ. 
     diceDoc.src =  "dice-" + diceNumber + ".png";
-  });
+
+if (diceNumber !== 1 ) {
+    // 1- ээс  ялгаатай тоо буулаа. Тоглогчид нэмж өглөө
+    roundScore = roundScore + diceNumber;
+    document.getElementById('current-' + activePlayer).textContent = roundScore;  
+} else {
+    //Тоглогчийн ээлжиндээ цуглуулсан оноог 0 болгоно.
+    roundScore = 0 ;
+    document.getElementById('current-' + activePlayer).textContent = 0;
+    // Тоглогчийн ээлжийг энэ хэсэгт сольно.
+    // Хэрэв идэвхтэй тоглогч 0 байвал идэвхтэй тоглогчийг 1 нэг болгоно.
+    // Үгүй бол идэвхтэй тоглогчийг 0 болго:
+    activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0)
+//    Улаан цэгийг шилжүүлэхЫ
+document.querySelector('.player-0-panel').classList.toggle("active");
+document.querySelector('.player-1-panel').classList.toggle("active");
+//Шоог түр алга болгон
+diceDoc.style.display = "none";
+}
+});
